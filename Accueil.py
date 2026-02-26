367e81aa49a42ce705efb91da92d286866606e45
nrivollet <nicolas.rivollet@gmail.com>
2026-02-26 19:16:02 +0100

    first commit

---
+++ Accueil.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Nicolas Rivollet | Portfolio Actuariat", layout="wide")
+
+st.title("🚀 Nicolas Rivollet")
+st.subheader("Expertise Actuarielle & Risk Management")
+
+st.markdown("""
+---
+**Ingénieur CentraleSupélec | MS ESCP | Actuaire Certifié**
+Ancien Head of Risk Management, je développe ici des outils de pilotage stratégique des risques.
+""")
+
+st.info("Utilisez le menu à gauche pour explorer les modules techniques.")
