506a67aa28f0eae7f21bfd88e8e4b0030c40faf9
Nicolas <nicolasrivollet@users.noreply.github.com>
2026-02-27 13:26:29 +0000

    reverse back title

--- Accueil.py
+++ Accueil.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Nicolas Rivollet | Portfolio Actuariat", layout="wide")
 
-st.title("🚀 Nicolas Rivollet zbozf")
+st.title("🚀 Nicolas Rivollet")
 st.subheader("Expertise Actuarielle & Risk Management")
 
 st.markdown("""
